d9ed01195704ecb64d12c3acff877c44a4c20066
Steven Boutcher <34585031+boutchersj@users.noreply.github.com>
2018-01-20 11:59:15 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+ $("#searchButton").click(function(){
+   event.preventDefault();
+var input = $("#input").val();
+var url = "https://en.wikipedia.org/w/api.php?format=json&action=opensearch&search=" + input + "&format=json&callback=?";
+
+   $.ajax({
+     type: "GET",
+     url: url,
+     async: false,
+     dataType: "json",
+     success: function(data){
+       $("ul").html("");
+       for (i=0; i<data[1].length; i++){
+$("ul").append("<li><a href=" + data[3][i] + "> \n" + data[1][i] + "==>" + data[2][i] + "</a></li>");
+       };
+     },
+     error: function(errorMessage){
+     alert("Ajax call failed!");
+   }
+   });
+       
+ });
+ });
