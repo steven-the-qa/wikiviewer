0dbc6fce88b69a13d40aec26d583beb98c4f3749
Steven Boutcher <sjboutcher@gmail.com>
2018-10-16 19:58:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var url = "https://en.wikipedia.org/w/api.php?format=json&action=opensearch&sear
      success: function(data){
        $("ul").html("\n\n\n");
        for (i=0; i<data[1].length; i++){
-$("ul").append("<li><a href=" + data[3][i] + ">" + "<strong>" + data[1][i] + "</strong>" + "</a>" + " --- \t\t" + data[2][i] + "</li>");
+$("ul").append("&emsp;" + "<li style='text-decoration: none'><a href=" + data[3][i] + ">" + "<strong>" + data[1][i] + "</strong>" + "</a>" + "&mdash;" + data[2][i] + "</li><br>");
        };
      },
      error: function(errorMessage){
